983c3e60afa97894a7d4497452969e035a94eb93
DeZo <zakaria.bouzouf@gmail.com>
2023-01-03 19:59:51 +0100

    Creation the the first bases of the Project

---
+++ app/head.js
@@ -0,0 +1,7 @@
+export default function Head() {
+	<>
+		<title>Metaversus</title>
+		<meta name='viewport' content='width=device-width, initial-scale=1' />
+		<link rel='icon' href='/favicon.ico' />
+	</>;
+}
