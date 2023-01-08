5f5fcb7870cd5b3be8afe811431ead51823e5900
DeZo <zakaria.bouzouf@gmail.com>
2023-01-08 21:50:47 +0100

    Adding the About section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,20 @@
-'use client';
+"use client";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from "framer-motion";
+
+import { textContainer, textVariant2 } from "../utils/motion.js";
+
+export const TypingText = ({ title, textStyle }) => (
+	<motion.p
+		variants={textContainer}
+		className={`font-normal text-[20px] text-secondary-white ${textStyle}`}
+	>
+		{Array.from(title).map((Letter, index) => (
+			<motion.span variants={textVariant2} key={index}>
+				{Letter === " " ? "\u00A0" : Letter}
+			</motion.span>
+		))}
+	</motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyle }) => <h2>Title Text</h2>;
