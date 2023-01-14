4495e716b8c8d4dc26fb9f971db17b3103b8ac36
DeZo <zakaria.bouzouf@gmail.com>
2023-01-14 20:03:56 +0100

    Adding the World section

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,58 @@
-'use client';
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion.js";
+import { TypingText, TitleText } from "../components";
 
 const World = () => (
-  <section>
-    World section
-  </section>
+	<section className={`${styles.paddings} relative z-10`}>
+		<motion.div
+			variants={staggerContainer}
+			initial='hidden'
+			whileInView='show'
+			viewport={{ once: "false", amount: 0.25 }}
+			className={`${styles.innerWidth} mx-auto flex flex-col `}
+		>
+			<motion.div
+				variants={fadeIn("left", "tween", 0.2, 1)}
+				className='flex-[0.75] flex justify-center flex-col'
+			>
+				<TypingText title='| People on the World' textStyle='text-center' />
+				<TitleText
+					title={
+						<>
+							Track friends around you and invite them to play together in the same
+							world
+						</>
+					}
+					textStyle='text-center'
+				/>
+				{/* <div className='mt-[31px] flex flex-row justify-around  gap-[24px]'>
+					{newFeatures.map((feature) => ({
+						<NewFeatures key={feature.title} {...feature} /> 
+					}))}
+				</div> */}
+			</motion.div>
+			<motion.div
+				variants={fadeIn("up", "tween", 0.3, 1)}
+				className='relative mt-[68px] flex w-full h-[550px]'
+			>
+				<img src='/map.png' alt='map' className='w-full h-full object-cover' />
+				<div className='absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+					<img src='/people-01.png' alt='people' className='w-full h-full' />
+				</div>
+				<div className='absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+					<img src='/people-02.png' alt='people' className='w-full h-full' />
+				</div>
+				<div className='absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+					<img src='/people-03.png' alt='people' className='w-full h-full' />
+				</div>
+			</motion.div>
+		</motion.div>
+	</section>
 );
 
 export default World;
